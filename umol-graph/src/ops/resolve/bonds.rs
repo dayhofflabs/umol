37a386dc56c80fe1aab4c1d0ddc867ecf7453d7a
Dmitrij Rappoport <dmrappoport@gmail.com>
2026-07-17 22:02:53 -0700

    Add transactions to bond resolvers

--- umol-graph/src/ops/resolve/bonds.rs
+++ umol-graph/src/ops/resolve/bonds.rs
@@ -3,7 +3,9 @@
 //! valence and aromaticity passes.
 
 use thiserror::Error;
-use umol_ast::ast::{Lattice, MoleculeAst, SpinStateAst, ValueAst};
+use umol_ast::ast::{
+    BondHandle, BondUpdate, Edit, Lattice, MoleculeAst, SpinStateAst, TransactionError, ValueAst,
+};
 use umol_utils::solution::Solution;
 
 #[derive(Clone, Debug, Default)]
@@ -13,55 +15,132 @@ pub struct BondsResolver;
 pub enum BondsContradiction {}
 
 #[derive(Debug, Error, Clone, PartialEq, Eq)]
-pub enum BondsError {}
+pub enum BondsError {
+    #[error(transparent)]
+    Transaction(#[from] TransactionError),
+}
 
 impl BondsResolver {
     pub fn new() -> Self {
         Self
     }
 
-    /// Fills charge and spin defaults. Uses maximum multiplicity for partially defined
-    /// spin states, otherwise uses closed-shell singlet.
+    /// Construct charge and spin default edits without mutating `ast`.
+    pub fn plan(&self, ast: &MoleculeAst) -> Vec<Edit> {
+        let mut edits = Vec::new();
+        for bond_id in ast.bonds().ids() {
+            let bond = ast.bond(bond_id).ast;
+            let mut selected_spin = bond.spin.clone();
+            let mut update = BondUpdate::default();
+            if matches!(bond.charge, ValueAst::Undetermined) {
+                update.charge = Some(ValueAst::Lit(0));
+            }
+            if selected_spin.is_undetermined() {
+                selected_spin = SpinStateAst::closed_shell();
+            } else {
+                selected_spin.high_spin_complete();
+            }
+            update.spin = bond.spin.difference_to(&selected_spin);
+            edits.extend(Edit::for_bond_update(
+                BondHandle::Id(bond_id),
+                bond,
+                &update,
+            ));
+        }
+        edits
+    }
+
+    /// Plan and atomically apply localized-bond defaults.
     pub fn resolve(
         &self,
         ast: &mut MoleculeAst,
     ) -> Result<Solution<(), BondsContradiction>, BondsError> {
-        ast.modify_bonds(|mut bond| {
-            if matches!(bond.charge, ValueAst::Undetermined) {
-                bond.charge = ValueAst::Lit(0);
-            }
-            if bond.spin.is_undetermined() {
-                bond.spin = SpinStateAst::closed_shell();
-            } else {
-                bond.spin.high_spin_complete();
-            }
-            bond
-        });
+        let edits = self.plan(ast);
+        let mut editor = ast.edit();
+        editor.transact(edits)?;
+        *ast = editor.build();
         Ok(Solution::Determined(()))
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use rstest::*;
-    use umol_ast::ast::BondId;
+    use rstest::rstest;
+    use umol_ast::ast::{BondFieldChange, BondId};
     use umol_ast::mol_dsl;
 
     use super::*;
 
     #[rstest]
-    #[case::undetermined(mol_dsl!(r#"{:atoms ["C" "C"] :bonds [{:atoms [0 1] :type "1"}]}"#), ValueAst::Lit(0), SpinStateAst::closed_shell())]
-    #[case::spin_undetermined(mol_dsl!(r#"{:atoms ["C" "C"] :bonds [{:atoms [0 1] :type "1#c+"}]}"#), ValueAst::Lit(1), SpinStateAst::closed_shell())]
-    #[case::charge_undetermined(mol_dsl!(r#"{:atoms ["C" "C"] :bonds [{:atoms [0 1] :type "1#u2#s1"}]}"#), ValueAst::Lit(0), SpinStateAst::from((2_u8, 1_u8)))]
-    #[case::unpaired_undetermined(mol_dsl!(r#"{:atoms ["C" "C"] :bonds [{:atoms [0 1] :type "1#s3"}]}"#), ValueAst::Lit(0), SpinStateAst::from((2_u8, 3_u8)))]
-    #[case::multiplicity_undetermined(mol_dsl!(r#"{:atoms ["C" "C"] :bonds [{:atoms [0 1] :type "1#u2"}]}"#), ValueAst::Lit(0), SpinStateAst::from((2_u8, 3_u8)))]
+    #[case::undetermined(
+        mol_dsl!(r#"{:atoms ["C" "C"] :bonds [[0 1 "1"]]}"#),
+        vec![
+            Edit::ModifyBondField {
+                id: BondHandle::Id(BondId(0)),
+                change: BondFieldChange::Charge {
+                    old: ValueAst::Undetermined,
+                    new: ValueAst::Lit(0),
+                },
+            },
+            Edit::ModifyBondField {
+                id: BondHandle::Id(BondId(0)),
+                change: BondFieldChange::Spin {
+                    old: SpinStateAst::default(),
+                    new: SpinStateAst::closed_shell(),
+                },
+            },
+        ]
+    )]
+    #[case::partial_spin(
+        mol_dsl!(r#"{:atoms ["C" "C"] :bonds [[0 1 "1#c+#s3"]]}"#),
+        vec![Edit::ModifyBondField {
+            id: BondHandle::Id(BondId(0)),
+            change: BondFieldChange::Spin {
+                old: SpinStateAst {
+                    unpaired: ValueAst::Undetermined,
+                    multiplicity: ValueAst::Lit(3),
+                },
+                new: SpinStateAst::from((2_u8, 3_u8)),
+            },
+        }]
+    )]
+    fn test_bonds_resolver_plan(#[case] molecule: MoleculeAst, #[case] expected: Vec<Edit>) {
+        assert_eq!(BondsResolver::new().plan(&molecule), expected);
+    }
+
+    #[rstest]
+    #[case::determined(mol_dsl!(r#"{:atoms ["C" "C"] :bonds [[0 1 "1#c+#u2#s1"]]}"#))]
+    fn test_bonds_resolver_plan_identity(#[case] molecule: MoleculeAst) {
+        assert_eq!(BondsResolver::new().plan(&molecule), Vec::new());
+    }
+
+    #[rstest]
+    #[case::partial_spin(
+        mol_dsl!(r#"{:atoms ["C" "C"] :bonds [[0 1 "1#s3"]]}"#),
+        mol_dsl!(r#"{:atoms ["C" "C"] :bonds [[0 1 "1#c0#u2#s3"]]}"#)
+    )]
     fn test_bonds_resolver_resolve(
-        #[case] mut mol: MoleculeAst,
-        #[case] charge: ValueAst,
-        #[case] spin: SpinStateAst,
+        #[case] mut molecule: MoleculeAst,
+        #[case] expected: MoleculeAst,
     ) {
-        BondsResolver::new().resolve(&mut mol).unwrap();
-        assert_eq!(mol.bond(BondId(0)).ast.charge, charge);
-        assert_eq!(mol.bond(BondId(0)).ast.spin, spin);
+        assert_eq!(
+            BondsResolver::new().resolve(&mut molecule),
+            Ok(Solution::Determined(()))
+        );
+        assert_eq!(molecule, expected);
+    }
+
+    #[rstest]
+    fn test_bonds_resolver_plan_stale() {
+        let mut molecule = mol_dsl!(r#"{:atoms ["C" "C" "C"] :bonds [[0 1 "1"] [1 2 "1"]]}"#);
+        let edits = BondsResolver::new().plan(&molecule);
+        molecule.bond_mut(BondId(1)).ast.charge = ValueAst::Lit(9);
+        let expected = molecule.clone();
+        let mut editor = molecule.edit();
+        assert_eq!(
+            editor.transact(edits),
+            Err(TransactionError::OldStateMismatch)
+        );
+        assert_eq!(editor.build(), expected);
     }
 }
--- umol-graph/src/ops/resolve/multicenter.rs
+++ umol-graph/src/ops/resolve/multicenter.rs
@@ -2,7 +2,10 @@
 //! defaults parallel to `BondsResolver`.
 
 use thiserror::Error;
-use umol_ast::ast::{Lattice, MoleculeAst, SpinStateAst, ValueAst};
+use umol_ast::ast::{
+    Edit, Lattice, MoleculeAst, MulticenterBondHandle, MulticenterBondUpdate, SpinStateAst,
+    TransactionError, ValueAst,
+};
 use umol_utils::solution::Solution;
 
 #[derive(Clone, Debug, Default)]
@@ -12,56 +15,151 @@ pub struct MulticenterBondsResolver;
 pub enum MulticenterBondsContradiction {}
 
 #[derive(Debug, Error, Clone, PartialEq, Eq)]
-pub enum MulticenterBondsError {}
+pub enum MulticenterBondsError {
+    #[error(transparent)]
+    Transaction(#[from] TransactionError),
+}
 
 impl MulticenterBondsResolver {
     pub fn new() -> Self {
         Self
     }
 
+    /// Construct charge and spin default edits without mutating `ast`.
+    pub fn plan(&self, ast: &MoleculeAst) -> Vec<Edit> {
+        let mut edits = Vec::new();
+        for bond_id in ast.multicenter_bonds().ids() {
+            let bond = ast.multicenter_bond(bond_id).ast;
+            let mut selected_spin = bond.spin.clone();
+            let mut update = MulticenterBondUpdate::default();
+            if matches!(bond.charge, ValueAst::Undetermined) {
+                update.charge = Some(ValueAst::Lit(0));
+            }
+            if selected_spin.is_undetermined() {
+                selected_spin = SpinStateAst::closed_shell();
+            } else {
+                selected_spin.high_spin_complete();
+            }
+            update.spin = bond.spin.difference_to(&selected_spin);
+            edits.extend(Edit::for_multicenter_bond_update(
+                MulticenterBondHandle::Id(bond_id),
+                bond,
+                &update,
+            ));
+        }
+        edits
+    }
+
+    /// Plan and atomically apply multicenter-bond defaults.
     pub fn resolve(
         &self,
         ast: &mut MoleculeAst,
     ) -> Result<Solution<(), MulticenterBondsContradiction>, MulticenterBondsError> {
-        ast.modify_multicenter_bonds(|mut bond| {
-            if matches!(bond.charge, ValueAst::Undetermined) {
-                bond.charge = ValueAst::Lit(0);
-            }
-            if bond.spin.is_undetermined() {
-                bond.spin = SpinStateAst::closed_shell();
-            } else {
-                bond.spin.high_spin_complete();
-            }
-            bond
-        });
+        let edits = self.plan(ast);
+        let mut editor = ast.edit();
+        editor.transact(edits)?;
+        *ast = editor.build();
         Ok(Solution::Determined(()))
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use rstest::*;
-    use umol_ast::ast::{MulticenterBondId, SpinStateAst, ValueAst};
+    use rstest::rstest;
+    use umol_ast::ast::{MulticenterBondFieldChange, MulticenterBondId};
     use umol_ast::mol_dsl;
 
     use super::*;
 
     #[rstest]
-    #[case::undetermined(mol_dsl!(r#"{:atoms ["B" "H" "B"] :multicenter-bonds [{:atoms [0 1 2] :type "[1, 0, 1]"}]}"#), ValueAst::Lit(0), SpinStateAst::closed_shell())]
-    #[case::spin_undetermined(mol_dsl!(r#"{:atoms ["B" "H" "B"] :multicenter-bonds [{:atoms [0 1 2] :type "[1, 0, 1]#c-"}]}"#), ValueAst::Lit(-1), SpinStateAst::closed_shell())]
-    #[case::charge_undetermined(mol_dsl!(r#"{:atoms ["B" "H" "B"] :multicenter-bonds [{:atoms [0 1 2] :type "[1, 0, 1]#u2#s1"}]}"#), ValueAst::Lit(0), SpinStateAst::from((2_u8, 1_u8)))]
-    #[case::unpaired_undetermined(mol_dsl!(r#"{:atoms ["B" "H" "B"] :multicenter-bonds [{:atoms [0 1 2] :type "[1, 0, 1]#s3"}]}"#), ValueAst::Lit(0), SpinStateAst::from((2_u8, 3_u8)))]
-    #[case::multiplicity_undetermined(mol_dsl!(r#"{:atoms ["B" "H" "B"] :multicenter-bonds [{:atoms [0 1 2] :type "[1, 0, 1]#u2"}]}"#), ValueAst::Lit(0), SpinStateAst::from((2_u8, 3_u8)))]
-    fn test_multicenter_bonds_resolver_fills_undetermined_charge_and_spin(
-        #[case] mut mol: MoleculeAst,
-        #[case] charge: ValueAst,
-        #[case] spin: SpinStateAst,
+    #[case::undetermined(
+        mol_dsl!(r#"{:atoms ["B" "H" "B"]
+                       :multicenter-bonds [{:atoms [0 1 2] :type "[1, 0, 1]"}]}"#),
+        vec![
+            Edit::ModifyMulticenterBondField {
+                id: MulticenterBondHandle::Id(MulticenterBondId(0)),
+                change: MulticenterBondFieldChange::Charge {
+                    old: ValueAst::Undetermined,
+                    new: ValueAst::Lit(0),
+                },
+            },
+            Edit::ModifyMulticenterBondField {
+                id: MulticenterBondHandle::Id(MulticenterBondId(0)),
+                change: MulticenterBondFieldChange::Spin {
+                    old: SpinStateAst::default(),
+                    new: SpinStateAst::closed_shell(),
+                },
+            },
+        ]
+    )]
+    #[case::partial_spin(
+        mol_dsl!(r#"{:atoms ["B" "H" "B"]
+                       :multicenter-bonds [{:atoms [0 1 2] :type "[1, 0, 1]#c-#s3"}]}"#),
+        vec![Edit::ModifyMulticenterBondField {
+            id: MulticenterBondHandle::Id(MulticenterBondId(0)),
+            change: MulticenterBondFieldChange::Spin {
+                old: SpinStateAst {
+                    unpaired: ValueAst::Undetermined,
+                    multiplicity: ValueAst::Lit(3),
+                },
+                new: SpinStateAst::from((2_u8, 3_u8)),
+            },
+        }]
+    )]
+    fn test_multicenter_bonds_resolver_plan(
+        #[case] molecule: MoleculeAst,
+        #[case] expected: Vec<Edit>,
     ) {
-        MulticenterBondsResolver::new().resolve(&mut mol).unwrap();
+        assert_eq!(MulticenterBondsResolver::new().plan(&molecule), expected);
+    }
+
+    #[rstest]
+    #[case::determined(mol_dsl!(r#"{:atoms ["B" "H" "B"]
+        :multicenter-bonds [{:atoms [0 1 2] :type "[1, 0, 1]#c-#u2#s1"}]}"#))]
+    fn test_multicenter_bonds_resolver_plan_identity(#[case] molecule: MoleculeAst) {
+        assert_eq!(MulticenterBondsResolver::new().plan(&molecule), Vec::new());
+    }
+
+    #[rstest]
+    #[case::partial_spin(
+        mol_dsl!(r#"{:atoms ["B" "H" "B"]
+                       :multicenter-bonds [{:atoms [0 1 2] :type "[1, 0, 1]#s3"}]}"#),
+        mol_dsl!(r#"{:atoms ["B" "H" "B"]
+                       :multicenter-bonds [{:atoms [0 1 2] :type "[1, 0, 1]#c0#u2#s3"}]}"#)
+    )]
+    fn test_multicenter_bonds_resolver_resolve(
+        #[case] mut molecule: MoleculeAst,
+        #[case] expected: MoleculeAst,
+    ) {
         assert_eq!(
-            mol.multicenter_bond(MulticenterBondId(0)).ast.charge,
-            charge
+            MulticenterBondsResolver::new().resolve(&mut molecule),
+            Ok(Solution::Determined(()))
         );
-        assert_eq!(mol.multicenter_bond(MulticenterBondId(0)).ast.spin, spin);
+        assert_eq!(molecule, expected);
+    }
+
+    #[rstest]
+    fn test_multicenter_bonds_resolver_plan_stale() {
+        let mut molecule = mol_dsl!(
+            r#"{
+            :atoms ["B" "H" "B" "H" "B"]
+            :multicenter-bonds [
+                {:atoms [0 1 2] :type "[1, 0, 1]"}
+                {:atoms [2 3 4] :type "[1, 0, 1]"}
+            ]
+        }"#
+        );
+        let edits = MulticenterBondsResolver::new().plan(&molecule);
+        molecule
+            .multicenter_bond_mut(MulticenterBondId(1))
+            .ast
+            .charge = ValueAst::Lit(9);
+        let expected = molecule.clone();
+        let mut editor = molecule.edit();
+        assert_eq!(
+            editor.transact(edits),
+            Err(TransactionError::OldStateMismatch)
+        );
+        assert_eq!(editor.build(), expected);
     }
 }
