df2048450632477f10e0755719c60006696eea7b
Dmitrij Rappoport <dmrappoport@gmail.com>
2026-08-16 10:24:39 -0700

    Remove constructor introspection from python 3.9 tests

--- umol-py/tests/conftest.py
+++ umol-py/tests/conftest.py
@@ -3,5 +3,5 @@ import sys
 
 collect_ignore = []
 if sys.version_info < (3, 10):
+    collect_ignore.append("test_class_constructor_signatures.py")
     collect_ignore.append("test_structural_pattern_matching.py")
-
